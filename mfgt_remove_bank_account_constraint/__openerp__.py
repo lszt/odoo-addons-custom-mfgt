8dcb4debc58ccee63a9d3a23e0e1810764c48722
mncoon <mathias.neef@syscoon.com>
2017-04-24 18:13:08 +0200

    [UPD] add new module to fix bank account constraint

---
+++ mfgt_remove_bank_account_constraint/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : 'MFGT Remove Bank Account Constraint',
+    'version': '9.0.1.0',
+    'website' : 'http://syscoon.com',
+    'category': 'Account',
+    'description': """
+Removes Constraint for Bank Account on BVR-Payment
+    """,
+    'depends' : [
+        'base',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
