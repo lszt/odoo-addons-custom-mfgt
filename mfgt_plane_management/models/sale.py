e2750118494dcf1cfb62641217a1d5d4f627b3c5
mncoon <mathias.neef@syscoon.com>
2017-05-06 06:31:53 +0200

    [UPD] changes in model and reports

--- mfgt_plane_management/models/sale.py
+++ mfgt_plane_management/models/sale.py
@@ -8,3 +8,16 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     plane_id = fields.Many2one('mfgt.plane.management', string='Plane', readonly=True, states={'draft': [('readonly', False)]})
+    plane_special_information = fields.Text('Special Information')
+
+    @api.onchange('partner_id')
+    def select_planes_for_partner(self):
+        res = {}
+        if self.partner_id:
+            res['domain'] = {'plane_id': [('owner_id', '=', self.partner_id.id)]}
+        return res
+
+    @api.onchange('plane_id')
+    def add_info_to_information(self):
+        if self.plane_id:
+            self.plane_special_information = self.plane_id.notes
