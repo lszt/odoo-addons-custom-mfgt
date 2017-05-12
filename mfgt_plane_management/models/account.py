0431e830acac359416aaf45ea57d0304f1da6899
mncoon <mathias.neef@syscoon.com>
2017-05-12 10:13:39 +0200

    [UPD] add plane details in invice

---
+++ mfgt_plane_management/models/account.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from openerp import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    sale_id = fields.Many2one('sale.order', compute='_select_sale_order', string='Sale Order', store=True)
+
+    @api.depends('origin')
+    @api.multi
+    def _select_sale_order(self):
+        for rec in self:
+            if rec.origin:
+                sale_order = self.env['sale.order'].search([('name', '=', rec.origin)])
+                if sale_order:
+                    rec.sale_id = sale_order[0].id
+        return
