87087f152a17358cd0a68661fb38405e69ef9c76
mncoon <mathias.neef@syscoon.com>
2018-09-05 19:03:37 +0200

    [UPD] add module to set back invoices with total amount zero to state open

---
+++ mfgt_invoice_open/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : 'MFGT Set Invoice back to Open',
+    'version': '9.0.1.0',
+    'website' : 'http://syscoon.com',
+    'category': 'Accounting',
+    'description': """
+Mangemant Tool for Aircraft Planes
+    """,
+    'depends' : [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
