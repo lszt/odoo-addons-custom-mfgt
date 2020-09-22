4b0c678aaeb0ec314fc4ca2607e05c45760b6c1d
Mathias Neef <mathias.neef@syscoon.com>
2020-09-22 15:38:39 +0200

    [UPD] increas font size on product label

--- profile_mfgt/__manifest__.py
+++ profile_mfgt/__manifest__.py
@@ -3,7 +3,7 @@
 # this module contains the full copyright notices and license terms.
 {
     'name': 'Profile MFGT - Motorfluggruppe Thurgau',
-    'version': '12.0.0.1.7',
+    'version': '12.0.0.1.8',
     'author': 'Motorfluggruppe Thurgau MFGT',
     'category': 'Custom',
     'website': 'https://www.mfgt.ch/',
