fa085909f3e6e0247178cbfadf1a6498cfda362b
Philipp Hug <philipp@hug.cx>
2020-09-28 13:57:44 +0200

    increase font size

--- profile_mfgt/__manifest__.py
+++ profile_mfgt/__manifest__.py
@@ -3,7 +3,7 @@
 # this module contains the full copyright notices and license terms.
 {
     'name': 'Profile MFGT - Motorfluggruppe Thurgau',
-    'version': '12.0.0.1.10',
+    'version': '12.0.0.1.11',
     'author': 'Motorfluggruppe Thurgau MFGT',
     'category': 'Custom',
     'website': 'https://www.mfgt.ch/',
