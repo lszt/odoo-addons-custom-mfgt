1c6890d7e382f1ea756fdcb6c8942ea44255f902
Mathias Neef <mathias.neef@syscoon.com>
2020-09-29 07:51:13 +0200

    [UPD] change top margin on product label

--- profile_mfgt/__manifest__.py
+++ profile_mfgt/__manifest__.py
@@ -3,7 +3,7 @@
 # this module contains the full copyright notices and license terms.
 {
     'name': 'Profile MFGT - Motorfluggruppe Thurgau',
-    'version': '12.0.0.1.8',
+    'version': '12.0.0.1.9',
     'author': 'Motorfluggruppe Thurgau MFGT',
     'category': 'Custom',
     'website': 'https://www.mfgt.ch/',
