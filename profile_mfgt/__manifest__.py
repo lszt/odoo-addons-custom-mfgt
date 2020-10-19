c6ab28d915db820911f2437087e2140e0fc20b30
Philipp Hug <philipp@hug.cx>
2020-10-19 08:53:15 +0200

    drop barcode

--- profile_mfgt/__manifest__.py
+++ profile_mfgt/__manifest__.py
@@ -3,7 +3,7 @@
 # this module contains the full copyright notices and license terms.
 {
     'name': 'Profile MFGT - Motorfluggruppe Thurgau',
-    'version': '12.0.0.1.13',
+    'version': '12.0.0.1.14',
     'author': 'Motorfluggruppe Thurgau MFGT',
     'category': 'Custom',
     'website': 'https://www.mfgt.ch/',
