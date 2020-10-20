c8782eeeafc56d7e3d8cc0c53fd43193b5dcc0ff
Philipp Hug <philipp@hug.cx>
2020-10-20 11:32:59 +0200

    fix columns

--- profile_mfgt/__manifest__.py
+++ profile_mfgt/__manifest__.py
@@ -3,7 +3,7 @@
 # this module contains the full copyright notices and license terms.
 {
     'name': 'Profile MFGT - Motorfluggruppe Thurgau',
-    'version': '12.0.0.1.16',
+    'version': '12.0.0.1.17',
     'author': 'Motorfluggruppe Thurgau MFGT',
     'category': 'Custom',
     'website': 'https://www.mfgt.ch/',
