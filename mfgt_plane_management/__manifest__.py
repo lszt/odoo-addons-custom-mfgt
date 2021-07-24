552deb67328909ac322b4d54fc73c080dff16f14
Philipp Hug <philipp@hug.cx>
2021-07-25 02:15:43 +0400

    bump version

--- mfgt_plane_management/__manifest__.py
+++ mfgt_plane_management/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name' : 'MFGT Plane Management',
-    'version': '12.0.1.2',
+    'version': '12.0.1.3',
     'website' : 'http://syscoon.com',
     'category': 'Sales',
     'description': """
